eb0948608445159334029c529d0bdb0582ce664c
RokuJS <shiuxcode@gmail.com>
2025-07-28 10:22:10 -0500

    Add dependency installation step in docker-entrypoint.sh

--- map_my_world/docker-entrypoint.sh
+++ map_my_world/docker-entrypoint.sh
@@ -2,6 +2,10 @@
 
 alias uv="/root/.local/bin/uv"
 
+# Install dependencies
+echo "Installing dependencies..."
+uv sync
+
 # run server
 echo "Starting FastAPI server..."
 uv run fastapi dev main.py --port 8000 --host 0.0.0.0
