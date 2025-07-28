258c6bad69a27af40cb2c487f75ac3b6d2a2de4a
RokuJS <shiuxcode@gmail.com>
2025-07-28 11:19:39 -0500

    Remove dependency installation step from docker-entrypoint.sh and add venv-data volume in docker-compose.yml

--- map_my_world/docker-entrypoint.sh
+++ map_my_world/docker-entrypoint.sh
@@ -2,10 +2,6 @@
 
 alias uv="/root/.local/bin/uv"
 
-# Install dependencies
-echo "Installing dependencies..."
-uv sync
-
 # run server
 echo "Starting FastAPI server..."
 uv run fastapi dev main.py --port 8000 --host 0.0.0.0
